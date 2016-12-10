681c3c80a79bf8333a0f58d8f271aab7d68fc232
Leon <info@leon.vankammen.eu>
2016-12-11 00:38:57 +0100

    fixed put/update operation

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
 (function(dirname,Collection) {
 
 	var monkeypatch = require('monkeypatch')
+  var debug = require('debug')('dpd-filebased-mongodb');
   var mongo = false 
   var ObjectId = false
+  var instance = false
 
   // patch require()             
   monkeypatch( require('module').prototype,'require', function(original, modname ){
+
+    // this patch will redirect mongodb to mongo-mock
     if( modname == 'mongodb' ){
       if( !mongo ){
         mongo = original('mongo-mock')
         mongo.max_delay = 0
         var MongoClient = mongo.MongoClient;
-        var dbfile = process.env.MONGO_DB_FILE || "data/mongodb.js";
+        var dbfile = process.env.MONGO_DB_FILE || process.cwd()+"/mongodb.js";
         MongoClient.persist= dbfile 
 				try{
 					require(dbfile)
 				}catch (e){
-					require('fs').writeFileSync(dbfile,"module.export = {}")
+					require('fs').writeFileSync(dbfile,"module.exports = {}")
 				}
 				MongoClient.load(dbfile)
-        console.log("mongodb redirected to '"+dbfile+"'")
+        debug("mongodb redirected to '"+dbfile+"'")
       }
+      
       return mongo 
     } 
+
+    // nasty fix for an edgecase with mongo-mock to make update() work
+    if( modname == './collection.js' ){
+      return function(dbname,server){
+        var collection = original(modname)(dbname,server)  
+        var update = collection.update 
+        collection.update = function(){
+          if( arguments.length == 5 && arguments[4] == false ){
+            var args = Array.prototype.slice.call(arguments,0,-1)
+            var cb = args[3]
+            args[3] = function(err,result){
+              cb( err, {result:result}) 
+            }
+            return update.apply({},args)
+          }else return update.apply({}, args )
+        }
+        return collection
+      }
+    }
 
 		// patch bson-objectid to support 16-char idstrings
     if( modname == 'bson-objectid' ){
@@ -51,7 +75,6 @@
 				return v
 			}
 		}
-    
 		return original(modname)
   }) 
   
