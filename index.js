1db6a4aa6cfc7de5fc9ddafc5d7e12fe0edaeda2
Leon <info@leon.vankammen.eu>
2016-12-16 13:11:06 +0100

    minor bugfix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-(function(dirname,Collection) {
+(function(dirname, Collection) {
 
-	var monkeypatch = require('monkeypatch')
+  var monkeypatch = require('monkeypatch')
   var debug = require('debug')('dpd-filebased-mongodb');
   var mongo = false 
   var ObjectId = false
@@ -18,24 +18,28 @@
         var MongoClient = mongo.MongoClient;
         var dbfile = process.env.MONGO_DB_FILE || process.cwd()+"/mongodb.js";
         MongoClient.persist= dbfile 
-				try{
-					fs.readFileSync(dbfile)
+        try{
+          fs.readFileSync(dbfile)
           console.log("reading "+dbfile)
-				}catch (e){
+        }catch (e){
           console.log("creating "+dbfile)
-					fs.writeFileSync(dbfile,"module.exports = {}")
-				}
+          fs.writeFileSync(dbfile,"module.exports = {}")
+        }
         MongoClient.load(dbfile)
       }
       
       return mongo 
     } 
 
-    // nasty fix for an edgecases with mongo-mock to make update() work
+    // nasty fix for mongo-mock edgecases 
     if( modname == './collection.js' ){
+
       return function(dbname,server){
-        var collection = original(modname)(dbname,server)  
+        var mod = original(modname)
+        collection = new mod(dbname,server)  
         var update = collection.update 
+
+        // workaround for update() 
         collection.update = function(){
           if( arguments.length == 5 && arguments[4] == false ){
             var args = Array.prototype.slice.call(arguments,0,-1)
@@ -47,19 +51,31 @@
           }else return update.apply({}, arguments )
         }
 
-				monkeypatch(collection,'find',function(originalFind){
-					var args = []
-					for( var i in arguments ) 
-						if( i > 0 ) args[i-1] = arguments[i]
-					if( args[1] == undefined ) args[1] = {}
-					return originalFind.apply(this,args)
-				})
+        // mongo-mock crashes on undefined find-arguments
+        monkeypatch(collection,'find',function(originalFind){
+          var args = []
+          for( var i in arguments ) 
+            if( i > 0 ) args[i-1] = arguments[i]
+          if( args[1] == undefined ) args[1] = {}
+          return originalFind.apply(this,args)
+        })
+
+        // mongo-mock remove() does not support '$in'
+        monkeypatch(collection,'remove',function(originalRemove, selector, opts, callback){
+          var args = []
+          for( var i in arguments ) if( i > 0 ) args[i-1] = arguments[i]
+          var selector = arguments[0]
+          if( selector && selector._id && selector._id['$in'] ) {
+            selector._id = selector._id['$in'].pop()
+          }
+          return originalRemove.apply(this,arguments)
+        })
 
         return collection
       }
     }
 
-		// patch bson-objectid to support 16-char idstrings
+    // patch bson-objectid to support 16-char idstrings
     if( modname == 'bson-objectid' ){
       if( !ObjectId ){
         var o = original('bson-objectid')
@@ -71,7 +87,7 @@
       return ObjectId
     }
 
-		return original(modname)
+    return original(modname)
   }) 
   
-})(__dirname, require('deployd/lib/resources/collection'))
+})(__dirname, require('deployd/lib/resources/collection') )
